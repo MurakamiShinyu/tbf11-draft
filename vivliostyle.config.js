e7959ee432e1cd8eab94edf2479910fb678c645d
Shinyu Murakami <murakami@vivliostyle.org>
2022-06-30 14:37:59 +0900

    Update vivliostyle.config.js

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -3,7 +3,7 @@ module.exports = {
   author: 'Vivliostyle ユーザー会', // default to 'author' in 'package.json' or undefined
   language: 'ja',
   // size: 'JIS-B5',
-  theme: 'theme/theme_screen.css', // .css or local dir or npm package. default to undefined
+  theme: '/theme/theme_screen.css', // .css or local dir or npm package. default to undefined
   entry: [
     {
       path: 'content/index.html',
